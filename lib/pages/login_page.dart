83c7aaf241487748c93bd3eb3c516bca445cd866
Ayan Gorain <ayan.gorain2021@vitbhopal.ac.in>
2022-07-16 17:37:49 +0530

    login page update

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -6,6 +6,8 @@ class Loginpage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.white,
+        body: Text("chitya")
+        ,
         
       
     );
