13e11fe2b311e6c4813ef2c7d73b2ef2c6ee9cb6
Ayan Gorain <ayan.gorain2021@vitbhopal.ac.in>
2022-10-01 16:09:06 +0530

    completed

--- lib/pages/commonpages/splash screen.dart
+++ lib/pages/commonpages/splash screen.dart
@@ -2,10 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import '../customer/navigation/main_page.dart';
-import '../customer/navigation/welcome_page.dart';
 import 'open_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class splashscreenpage extends StatefulWidget {
   const splashscreenpage({Key? key}) : super(key: key);
@@ -20,30 +17,8 @@ class _splashscreenpageState extends State<splashscreenpage> {
   void initState(){
     super.initState();
     Timer(Duration(seconds: 4),(){
-
-
-      try {
-        User? firebaseUserr = FirebaseAuth.instance.currentUser;
-        var user = firebaseUserr ?? null;
-
-        if (user?.uid != null) {
-          Navigator.of(context)
-              .pushReplacement(MaterialPageRoute(builder: (_)=>Openpage()));
-
-        }
-        else
-          {
-            Navigator.of(context)
-                .pushReplacement(MaterialPageRoute(builder: (_)=>BottomNavBar()));
-          }
-      } catch (e) {
-       // log(e.toString());
-      }
-
-
-
-
-
+      Navigator.of(context)
+          .pushReplacement(MaterialPageRoute(builder: (_)=>Openpage()));
     });
   }
   @override
